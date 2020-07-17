5d4ceb5db0942a5df7896074ebf451e3696ebb8d
jiawei <chia021@163.com>
2020-07-17 12:36:22 +0800

    AndroidFrame

--- app/src/main/java/cn/chia/docking/MainActivity.kt
+++ app/src/main/java/cn/chia/docking/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
             ARouter.getInstance().build("/halo/main").navigation()
         }
         bt_hdl.setOnClickListener {
-            L.i("上传测试")
+            L.i("Docking框架上传测试")
             ARouter.getInstance().build("/hdl/main").navigation()
         }
         /*
